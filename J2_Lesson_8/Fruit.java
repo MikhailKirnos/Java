142308c22edbe80082ee7b122ad7981bc4e0a224
MikhailKirnos <arduan312@mail.ru>
2020-12-18 12:57:49 +0300

    Домашняя работа №1 к Java3

---
+++ J2_Lesson_8/Fruit.java
@@ -0,0 +1,14 @@
+package J2_Lesson_8;
+
+abstract class Fruit {
+
+    private float weight;
+
+    public Fruit(float weight) {
+        this.weight = weight;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+}
